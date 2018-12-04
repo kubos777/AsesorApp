2326a4469ede6d9e22f4e1af8f52aa1baea36272
kubos <chd.jl@hotmail.com>
2018-12-04 17:55:40 -0600

    :smile: Subiendo últimos cambios, acomodando labels.

--- final/cellDetailViewController.swift
+++ final/cellDetailViewController.swift
@@ -5,7 +5,6 @@
 //  Created by Alonso de Gortari Rabiela on 11/25/18.
 //  Copyright © 2018 julianYjorge. All rights reserved.
 //
-
 import UIKit
 
 class cellDetailViewController: UIViewController {
@@ -16,8 +15,10 @@ class cellDetailViewController: UIViewController {
     @IBOutlet weak var usuarioOutlet: UILabel!
     @IBOutlet weak var fechaOutlet: UILabel!
     @IBOutlet weak var descripcionOutlet: UILabel!
-    
+    @IBOutlet weak var correoOutlet: UILabel!
+    @IBOutlet weak var nombreOutlet: UILabel!
     @IBOutlet weak var lugarOutlet: UILabel!
+    
     @IBAction func aceptarAction(_ sender: UIButton) {
         guard let user: Usuario = Usuario.getUserToken() else {
             return
@@ -37,20 +38,21 @@ class cellDetailViewController: UIViewController {
         usuarioOutlet.text = publicacion.usuario.usuario
         descripcionOutlet.text = publicacion.descripcion
         lugarOutlet.text = publicacion.lugar
+        nombreOutlet.text = publicacion.usuario.nombre
+        correoOutlet.text = publicacion.usuario.correo
         let dateformatter = DateFormatter()
-        dateformatter.dateFormat = "yyyy-MM-dd"
+        dateformatter.dateFormat = "dd-MM-yyyy"
         fechaOutlet.text = dateformatter.string(from: publicacion.fecha)
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
